03b23754e719ffee4bd6492053ada8094d4978e6
brittneypostma <brittney.postma@hotmail.com>
2021-05-18 13:22:39 -0400

    init

---
+++ svelte.config.js
@@ -0,0 +1,19 @@
+import path from 'path'
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte',
+    vite: {
+			resolve: {
+				alias: {
+					$utils: path.resolve('./src/utils'),
+          $components: path.resolve('./src/components')
+				}
+			}
+		}
+	}
+};
+
+export default config;
