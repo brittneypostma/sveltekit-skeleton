077e964498112326353a7821f81066d8a07e56d4
brittneypostma <brittney.postma@hotmail.com>
2021-05-19 12:47:09 -0400

    edited configs

--- svelte.config.js
+++ svelte.config.js
@@ -2,18 +2,18 @@ import path from 'path'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
+  kit: {
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: '#svelte',
     vite: {
-			resolve: {
-				alias: {
-					'@utils': path.resolve('./src/utils'),
-          '@components': path.resolve('./src/components')
-				}
-			}
-		}
-	}
-};
+      resolve: {
+        alias: {
+          '@utils': path.resolve('./src/utils'),
+          '@lib': path.resolve('./src/lib'),
+        },
+      },
+    },
+  },
+}
 
-export default config;
+export default config
