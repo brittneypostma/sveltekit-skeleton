c4df03530411d2ee4d215ac6e62d56210e7d05db
brittneypostma <brittney.postma@hotmail.com>
2021-05-19 12:47:09 -0400

    edited configs

--- svelte.config.js
+++ svelte.config.js
@@ -3,24 +3,18 @@ import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
-
-	kit: {
-		adapter: adapter(),
-
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
+  kit: {
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: '#svelte',
     vite: {
-			resolve: {
-				alias: {
-					'@utils': path.resolve('./src/utils'),
-          '@components': path.resolve('./src/components')
-				}
-			}
-		}
-	}
-};
+      resolve: {
+        alias: {
+          '@utils': path.resolve('./src/utils'),
+          '@lib': path.resolve('./src/lib'),
+        },
+      },
+    },
+  },
+}
 
-export default config;
+export default config
